391af280b300e257bd53393a86fa0e8739aa1d20
The-King-12345 <alexseidinger15@gmail.com>
2025-05-21 20:52:54 -0700

    Add win screen
    Separate code into separate Scenes for easier management

---
+++ scenes/gameScene.js
@@ -0,0 +1,107 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super("GameScene");
+    }
+    
+    preload() {
+        this.load.image("blue", "assets/blue.png");
+        this.load.image("green", "assets/green.png");
+        this.load.image("yellow", "assets/yellow.png");
+        this.load.image("red", "assets/red.png");
+        this.load.image("blueTxt", "assets/blueTxt.png");
+        this.load.image("greenTxt", "assets/greenTxt.png");
+        this.load.image("yellowTxt", "assets/yellowTxt.png");
+        this.load.image("redTxt", "assets/redTxt.png");
+        this.load.image("youTxt", "assets/youTxt.png");
+        this.load.image("winTxt", "assets/winTxt.png");
+        this.load.image("pushTxt", "assets/pushTxt.png");
+        this.load.image("stopTxt", "assets/stopTxt.png");
+        this.load.image("isTxt", "assets/isTxt.png");
+    }
+
+    create() {
+        arrowKeys = this.input.keyboard.createCursorKeys();
+        wasdKeys = this.input.keyboard.addKeys({
+            up: "W",
+            down: "S",
+            left: "A",
+            right: "D",
+            reset: "R"
+        });
+
+        // populate all sprites 
+        sprites.length = 0;
+
+        addSprite(this, 11, 8, "red");
+        addSprite(this, 12, 8, "red");
+        addSprite(this, 13, 8, "red");
+        addSprite(this, 14, 8, "red");
+        addSprite(this, 15, 8, "red");
+        addSprite(this, 16, 8, "red");
+        addSprite(this, 17, 8, "red");
+        addSprite(this, 18, 8, "red");
+        addSprite(this, 19, 8, "red");
+        addSprite(this, 20, 8, "red");
+        addSprite(this, 21, 8, "red");
+
+        addSprite(this, 11, 12, "red");
+        addSprite(this, 12, 12, "red");
+        addSprite(this, 13, 12, "red");
+        addSprite(this, 14, 12, "red");
+        addSprite(this, 15, 12, "red");
+        addSprite(this, 16, 12, "red");
+        addSprite(this, 17, 12, "red");
+        addSprite(this, 18, 12, "red");
+        addSprite(this, 19, 12, "red");
+        addSprite(this, 20, 12, "red");
+        addSprite(this, 21, 12, "red");
+        
+        addSprite(this, 16, 9, "yellow");
+        addSprite(this, 16, 10, "yellow");
+        addSprite(this, 16, 11, "yellow");
+        
+        addSprite(this, 20, 10, "green");
+        addSprite(this, 12, 10, "blue");
+
+        addText(this, 11, 6, "blueTxt", "noun");
+        addText(this, 12, 6, "isTxt", "is");
+        addText(this, 13, 6, "youTxt", "verb");
+        
+        addText(this, 19, 6, "greenTxt", "noun");
+        addText(this, 20, 6, "isTxt", "is");
+        addText(this, 21, 6, "winTxt", "verb");
+
+        addText(this, 11, 14, "redTxt", "noun");
+        addText(this, 12, 14, "isTxt", "is");
+        addText(this, 13, 14, "stopTxt", "verb");
+
+        addText(this, 19, 14, "yellowTxt", "noun");
+        addText(this, 20, 14, "isTxt", "is");
+        addText(this, 21, 14, "pushTxt", "verb");
+
+        updateEverything();
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(arrowKeys.up) || Phaser.Input.Keyboard.JustDown(wasdKeys.up)) {
+            startMove(0, -1);
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(arrowKeys.down) || Phaser.Input.Keyboard.JustDown(wasdKeys.down)) {
+            startMove(0, 1);
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(arrowKeys.left) || Phaser.Input.Keyboard.JustDown(wasdKeys.left)) {
+            startMove(-1, 0);
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(arrowKeys.right) || Phaser.Input.Keyboard.JustDown(wasdKeys.right)) {
+            startMove(1, 0);
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(wasdKeys.reset)) {
+            this.scene.restart();
+        }
+        
+    }
+}
---
+++ scenes/winScene.js
@@ -0,0 +1,26 @@
+class WinScene extends Phaser.Scene {
+    constructor() {
+        super("WinScene");
+    }
+
+    preload() {
+        this.load.image("blue", "assets/blue.png");
+    }
+
+    create() {
+        this.input.keyboard.on('keydown', () => {
+            this.scene.stop("WinScene")
+            this.scene.start("GameScene");
+        });
+
+
+        this.add.text(WIDTH/2,HEIGHT/2,"You Win!", 
+            { 
+                fontSize: "24px", 
+                fontStyle: "bold"
+            }
+        ).setOrigin(0.5);
+
+
+    }
+}
